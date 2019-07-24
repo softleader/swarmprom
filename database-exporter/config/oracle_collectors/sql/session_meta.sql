87c23b86e260b5eb4adda402bad451dd01fcf08e
Rhys <godjdc@gmail.com>
2019-07-24 18:11:21 +0800

    update oracle dashboard

---
+++ database-exporter/config/oracle_collectors/sql/session_meta.sql
@@ -0,0 +1,17 @@
+SELECT
+ss.sid "sid",
+ss.username "username",
+ss.machine "machine",
+ss.program "program",
+ss.blocking_session "blocking_session",
+ss.status "status",
+se.VALUE/100 "cpu_usage_seconds",
+1 "cnt_value"
+FROM
+v$session ss,
+v$sesstat se,
+v$statname sn
+WHERE ss.username iS NOT NULL
+AND se.statistic# = sn.statistic#
+AND sn.name like '%CPU used by this session%'
+AND se.sid = ss.sid
